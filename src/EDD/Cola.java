e49e109ffe80aca40f9c4af624a2e67ddd168cce
Nakami <natalia.rivas@correo.unimet.edu.ve>
2024-02-24 21:49:46 -0400

    solo puse las primitivas para las colas :construction:

---
+++ src/EDD/Cola.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDD;
+
+/**
+ *
+ * @author Natalia
+ */
+public class Cola {
+    private Nodo first;
+    private Nodo last;
+    private int size;
+
+    public Cola() {
+        this.first = null;
+        this.size = 0;
+    }
+
+    public Nodo getFirst() {
+        return first;
+    }
+
+    public void setFirst(Nodo first) {
+        this.first = first;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Nodo getLast() {
+        return last;
+    }
+
+    public void setLast(Nodo last) {
+        this.last = last;
+    }
+    
+    
+    public boolean empty() {
+       return getFirst()==null;
+    
+    }
+    
+    
+    public Nodo encolar(Character element) {
+        Nodo nuevo=new Nodo(element);
+        if (empty()) {
+            setFirst(nuevo);
+            setLast(nuevo);
+        
+        }
+        
+        else {
+           getLast().setNext(nuevo);
+           setLast(nuevo);
+        }
+        size ++;
+        return nuevo;
+    }
+    
+    public Nodo desencolar() {
+        if (!empty()) {
+            Nodo pointer= getFirst();
+            setFirst(pointer.getNext());
+            pointer.setNext(null);
+            size --;
+            return pointer;
+        }
+       return null; 
+    }
+    
+    
+    public void imprimir() {
+    
+        for (Nodo aux=getFirst(); aux!=null; aux=aux.getNext()) {
+            System.out.println(aux.getElement());
+        }
+    
+    }
+    
+    
+    
+}
---
+++ src/EDD/Nodo.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDD;
+
+/**
+ *
+ * @author Natalia
+ */
+public class Nodo {
+    
+    private Character element;
+    
+    private Nodo next;
+
+    public Nodo(Character element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    public int getElement() {
+        return element;
+    }
+
+    public void setElement(Character element) {
+        this.element = element;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+    
+}
